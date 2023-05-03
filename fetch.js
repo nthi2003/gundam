47dd38fee77a74cef1aacc37470ceaf3b6ecf7e4
nthi2003 <quocthi.dev@gmail.com>
2023-05-03 13:51:10 +0700

    new2

---
+++ fetch.js
@@ -0,0 +1,39 @@
+const item = document.querySelector('.list-item')
+fetch('./data.json')
+      .then(res => res.json())
+      .then(data => {
+        data.forEach(post => {
+                item.insertAdjacentHTML('beforeend', `
+                <div class="media">
+                      <div class="thumbnail">
+                        <a href=""><img src="${post.image}" alt=""></a>
+                      </div>
+                      <div class="hoverable">
+                        <ul>
+                          <li class="active"><a href=""><i class="ri-heart-line"></i></a></li>
+                          <li><a href=""><i class="ri-eye-line"></i></a></li>
+                          <li><a href=""><i class="ri-shuffle-line"></i></a></li>
+                        </ul>
+                      </div>
+                      <div class="discount circle flexcenter"><span>32%</span></div>
+                    </div>
+                    <div class="content">
+                      <div class="rating">
+                        <div class="stars"></div>
+                        <span class="mini-text">(2,548)</span>
+                      </div>
+                      <h3 class="main-links"><a href="">
+                          ${post.name}
+                        </a></h3>
+                      <div class="price">
+                        <span class="current">$ ${post.sale}</span>
+                        <span class="normal mini-text">$ ${post.price}</span>
+                      </div>
+                      <div class="mini-text">
+                        <p>2891 sold</p>
+                        <p>Free Shipping</p>
+                      </div>
+                    </div>
+                `);
+        })
+      })
--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function copyMenu (){
     var topNav = document.querySelector('.header-top .wrapper');
     var topPlace = document.querySelector('.off-canvas .thetop-nav');
     topPlace.innerHTML = topNav.innerHTML;
+    
 }
 copyMenu() ; 
-
 
 
 const swiper = new Swiper('.swiper', {
